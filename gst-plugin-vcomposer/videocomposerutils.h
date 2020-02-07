e30e853d47511ee85c6b2795efa5ef06065c0c9b
Petar G. Georgiev <petarg@codeaurora.org>
2020-02-07 16:25:33 +0200

    vcomposer: Video composer plugin
    Plug-in for video streams mixing.
    
    Change-Id: I05b78113de71fe8d8679f1b877d9a4d8ee9bee3e

---
+++ gst-plugin-vcomposer/videocomposerutils.h
@@ -0,0 +1,57 @@
+/*
+* Copyright (c) 2020, The Linux Foundation. All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are
+* met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above
+*       copyright notice, this list of conditions and the following
+*       disclaimer in the documentation and/or other materials provided
+*       with the distribution.
+*     * Neither the name of The Linux Foundation nor the names of its
+*       contributors may be used to endorse or promote products derived
+*       from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+* ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+* BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+* OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+* IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef __GST_VIDEO_COMPOSER_UTILS_H__
+#define __GST_VIDEO_COMPOSER_UTILS_H__
+
+#include <gst/gst.h>
+
+G_BEGIN_DECLS
+
+#define GST_PROPERTY_IS_MUTABLE_IN_CURRENT_STATE(pspec, state) \
+    ((pspec->flags & GST_PARAM_MUTABLE_PLAYING) ? (state <= GST_STATE_PLAYING) \
+        : ((pspec->flags & GST_PARAM_MUTABLE_PAUSED) ? (state <= GST_STATE_PAUSED) \
+            : ((pspec->flags & GST_PARAM_MUTABLE_READY) ? (state <= GST_STATE_READY) \
+                : (state <= GST_STATE_NULL))))
+
+#define GST_TYPE_VIDEO_COMPOSER_ROTATE \
+  (gst_video_composer_rotate_get_type())
+
+typedef enum {
+  GST_VIDEO_COMPOSER_ROTATE_NONE,
+  GST_VIDEO_COMPOSER_ROTATE_90_CW,
+  GST_VIDEO_COMPOSER_ROTATE_90_CCW,
+  GST_VIDEO_COMPOSER_ROTATE_180,
+} GstVideoComposerRotate;
+
+GType gst_video_composer_rotate_get_type (void);
+
+G_END_DECLS
+
+#endif // __GST_VIDEO_COMPOSER_UTILS_H__
