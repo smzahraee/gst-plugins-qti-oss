3ce1ed6215d27271ce9efea44c325c1dc4eb8e80
Nikolay Hristozov <nhrist@codeaurora.org>
2020-10-08 11:13:51 +0300

    qtioverlay: initialize a correct default destination rect
    Set initial value to the destination rect to prevent crash if not set
    
    Change-Id: I0eee36cc49ec5961048f89b950ebf0dec4282fc0

--- gst-plugin-overlay/gstoverlay.cc
+++ gst-plugin-overlay/gstoverlay.cc
@@ -58,10 +58,10 @@ G_DEFINE_TYPE (GstOverlay, gst_overlay, GST_TYPE_VIDEO_FILTER);
 #define DEFAULT_PROP_OVERLAY_POSE_COLOR  kColorLightGreen
 #define DEFAULT_PROP_OVERLAY_MASK_COLOR  kColorDarkGray
 
-#define DEFAULT_PROP_DEST_RECT_X      0
-#define DEFAULT_PROP_DEST_RECT_Y      0
-#define DEFAULT_PROP_DEST_RECT_WIDTH  0
-#define DEFAULT_PROP_DEST_RECT_HEIGHT 0
+#define DEFAULT_PROP_DEST_RECT_X      40
+#define DEFAULT_PROP_DEST_RECT_Y      40
+#define DEFAULT_PROP_DEST_RECT_WIDTH  200
+#define DEFAULT_PROP_DEST_RECT_HEIGHT 40
 
 
 /* This is initial value. Size is recalculated runtime and buffer is
