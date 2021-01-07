bff40b46dc3306d5e72ac6fdbd4b8c4fbace583c
Jie Zhou <zhojie@codeaurora.org>
2021-01-07 11:36:47 +0530

    Gstreamer Decoder/Encoder pluign based on Codec2 framework
    Change-Id: Ie43a3a4cc7cf1ab851632c9875de6f303023b7e6
    Signed-off-by: Jie Zhou <zhojie@codeaurora.org>

---
+++ gst-plugin-codec2/wrapper/c2ComponentInterfaceAdapter.cpp
@@ -0,0 +1,81 @@
+/*
+* Copyright (c) 2021, The Linux Foundation. All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are
+* met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above
+*       copyright notice, this list of conditions and the following
+*       disclaimer in the documentation and/or other materials provided
+*       with the distribution.
+*     * Neither the name of The Linux Foundation nor the names of its
+*       contributors may be used to endorse or promote products derived
+*       from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+* ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+* BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+* OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+* IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "c2ComponentInterfaceAdapter.h"
+
+namespace QTI {
+
+C2ComponentInterfaceAdapter::C2ComponentInterfaceAdapter(std::shared_ptr<C2ComponentInterface> compIntf) {
+
+    mCompIntf = std::move(compIntf);
+}
+
+C2ComponentInterfaceAdapter::~C2ComponentInterfaceAdapter() {
+
+    mCompIntf = nullptr;
+}
+
+C2String C2ComponentInterfaceAdapter::getName () const  {
+
+    return mCompIntf->getName();
+}
+
+c2_node_id_t C2ComponentInterfaceAdapter::getId () const  {
+
+    return mCompIntf->getId();
+}
+
+c2_status_t C2ComponentInterfaceAdapter::config (const std::vector<C2Param*> &stackParams, c2_blocking_t mayBlock) {
+
+    LOG_MESSAGE("Component interface (%p) configured", this);
+
+    c2_status_t result = C2_NO_INIT;
+    std::vector<std::unique_ptr<C2SettingResult>> failures;
+
+    result = mCompIntf->config_vb(stackParams, mayBlock, &failures);
+    if ((C2_OK != result) || (failures.size() != 0)) {
+        LOG_WARNING("Configuration failed(%d)", static_cast<int32_t>(result));
+    }
+
+    return result;
+}
+
+c2_status_t C2ComponentInterfaceAdapter::setComponent(std::weak_ptr<C2Component> comp) {
+
+    c2_status_t result = C2_NO_INIT;
+
+    if (!comp.expired()){
+        mConnectedComponent = comp;
+        result = C2_OK;
+    }
+
+    return result;
+}
+
+} // namespace QTI
---
+++ gst-plugin-codec2/wrapper/c2ComponentStoreAdapter.cpp
@@ -0,0 +1,103 @@
+/*
+* Copyright (c) 2021, The Linux Foundation. All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are
+* met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above
+*       copyright notice, this list of conditions and the following
+*       disclaimer in the documentation and/or other materials provided
+*       with the distribution.
+*     * Neither the name of The Linux Foundation nor the names of its
+*       contributors may be used to endorse or promote products derived
+*       from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+* ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+* BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+* OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+* IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "c2ComponentStoreAdapter.h"
+#include "c2ComponentAdapter.h"
+#include "c2ComponentInterfaceAdapter.h"
+
+namespace QTI {
+
+C2ComponentStoreAdapter::C2ComponentStoreAdapter(std::shared_ptr<C2ComponentStore> store) {
+
+    mStore = std::move(store);
+}
+
+C2ComponentStoreAdapter::~C2ComponentStoreAdapter() {
+
+    mStore = nullptr;
+}
+
+C2String C2ComponentStoreAdapter::getName() {
+
+    C2String name;
+
+    if (mStore) {
+        name = mStore->getName();
+    }
+
+    return name;
+}
+
+c2_status_t C2ComponentStoreAdapter::createComponent(C2String name, void **const component) {
+
+    c2_status_t result = C2_BAD_VALUE;
+    std::shared_ptr<C2Component> comp = nullptr;
+
+    result = mStore->createComponent(name, &comp);
+    if ((result == C2_OK) && (comp != nullptr)) {
+        C2ComponentAdapter* comp_adapter = new C2ComponentAdapter(comp);
+
+        if (comp_adapter) {
+            *component = comp_adapter;
+        }
+    }
+
+    return result;
+}
+
+c2_status_t C2ComponentStoreAdapter::createInterface (C2String name, void **const interface) {
+
+    c2_status_t result = C2_BAD_VALUE;
+    std::shared_ptr<C2ComponentInterface> compIntf = nullptr;
+
+    result = mStore->createInterface(name, &compIntf);
+    if (result == C2_OK) {
+
+        C2ComponentInterfaceAdapter* intf_adapter = new C2ComponentInterfaceAdapter(compIntf);
+        if (intf_adapter != nullptr) {
+ 
+            *interface = intf_adapter;
+        }
+    }
+
+    return result;
+}
+
+std::vector<std::shared_ptr<const C2Component::Traits>> C2ComponentStoreAdapter::listComponents() {
+
+    std::vector<std::shared_ptr<const C2Component::Traits>> result;
+
+    if (mStore) {
+        result = mStore->listComponents();
+    }
+
+    return result;
+}
+
+} // namespace QTI
