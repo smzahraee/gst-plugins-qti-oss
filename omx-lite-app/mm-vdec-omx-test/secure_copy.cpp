89a574bedb45e2d1ae52e1398f88c6dcad6e054a
Ferry Zhou <tianguiz@codeaurora.org>
2021-05-12 15:26:40 +0800

    mm-vdec-omx-test: Add secure playback feature
    Add a secure mode to implement secure playback feature.
    
    OMX IL client initializes OMX component as secure mode, then OMX can
    decode byte-stream frames in secure OMX input buffer allocated by
    OMX_AllocateBuffer() that uses libion to allocate ION secure memory,
    and output decoded YUV frames into OMX output buffer (same as input
    buffer but it uses libgbm to allocate ION secure memory) for display.
    However, this OMX lite app is just for decoding demo purpose only, so
    no display for the output buffers, just log the decoded YUV frames
    into a file.
    
    Use libcontentcopy to copy compressed frames into secure input buffers
    for secure decoding and copy decoded frames from secure output buffers
    into a YUV file for correctness checking.
    
    Change descriptions:
    1. add secure mode into CLI user interface
    2. add secure_copy module to wrap libcontentcopy
    3. set app name as smpcpyap64 to fix copy failure from secure to
       non-secure buffer
    4. refine output YUV code with both secure/nonsecure cases
    5. add non-secure input/output buffer management for secure mode
    6. allocate non-secure YUV output buffer after output port reconfig
    7. set thread name fbd_thread and ebd_thread for debug purpose
    8. remove unused code of OMX_BUFFERFLAG_EXTRADATA
    9. refine and optimize debug logs
    10. free pOutYUVBufHdrs
    
    Change-Id: If6a6a69feb98e7fc8c9ccd859d6dde8fa9e9defc
    Signed-off-by: Ferry Zhou <tianguiz@codeaurora.org>

---
+++ omx-lite-app/mm-vdec-omx-test/secure_copy.cpp
@@ -0,0 +1,139 @@
+/*-------------------------------------------------------------------
+Copyright (c) 2021, The Linux Foundation. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+--------------------------------------------------------------------*/
+
+#include "secure_copy.h"
+
+#include <iostream>
+using namespace std;
+
+/* Secure content copy library interfaces. */
+#define SCC_LibName "libcontentcopy.so"
+#define SCC_AppName "smpcpyap64"
+#define SCC_SetAppName "Content_Protection_Set_AppName"
+#define SCC_Init "Content_Protection_Copy_Init"
+#define SCC_Terminate "Content_Protection_Copy_Terminate"
+#define SCC_Copy "Content_Protection_Copy"
+
+typedef int (*secure_copy_set_app_name_t)(const char *name);
+typedef int (*secure_copy_init_t)(void **handle);
+typedef int (*secure_copy_func_t)(void *handle,
+            uint8_t *non_sec_buf, uint32_t non_sec_buf_len,
+            uint32_t sec_buf_fd, uint32_t sec_buf_offset, uint32_t *sec_buf_len,
+            int copy_dir);
+typedef int (*secure_copy_deinit_t)(void **handle);
+
+secure_copy::secure_copy() :
+        lib_cc_handle(nullptr), secure_handle(nullptr), secure_copy_set_app_name(nullptr),
+        secure_copy_init(nullptr), secure_copy_func(nullptr), secure_copy_deinit(nullptr)
+{
+    int ret = load_lib();
+    if (ret)
+        goto unload;
+
+    ret = (*secure_copy_set_app_name)(SCC_AppName);
+    if (ret) {
+        cerr << "Falied to set app name " << SCC_AppName << "ret=" << ret << endl;
+        goto unload;
+    }
+
+    ret = (*secure_copy_init)(&secure_handle);
+    if (ret) {
+        cerr << "Falied to init " << SCC_LibName << "ret=" << ret << endl;
+        goto unload;
+    }
+
+    return;
+
+unload:
+    unload_lib();
+}
+
+bool
+secure_copy::copy(uint8_t *non_sec_buf, size_t non_sec_buf_len, int sec_buf_fd,
+                  size_t sec_buf_offset, size_t *sec_buf_len, int copy_dir)
+{
+    if (nullptr == secure_copy_func)
+        return false;
+
+    uint32_t len = (uint32_t)non_sec_buf_len;
+    uint32_t off = (uint32_t)sec_buf_offset;
+    uint32_t sec_fd = (uint32_t)sec_buf_fd;
+    uint32_t *sec_len = (uint32_t *)sec_buf_len;
+
+    int ret = (*secure_copy_func)(secure_handle, non_sec_buf, len,
+                                  sec_fd, off, sec_len, copy_dir);
+    if (ret) {
+        cerr << "Secure copy failed, ret=" << ret << endl;
+        return false;
+    }
+
+    return true;
+}
+
+secure_copy::~secure_copy()
+{
+    if (secure_copy_deinit && secure_handle)
+        (*secure_copy_deinit)(&secure_handle);
+    unload_lib();
+}
+
+int secure_copy::load_lib(void)
+{
+    lib_cc_handle = dlopen(SCC_LibName, RTLD_NOW);
+    if (nullptr == lib_cc_handle) {
+        cerr << "Falied to open " << SCC_LibName << ", " << dlerror() << endl;
+        return -1;
+    }
+
+    secure_copy_set_app_name = (secure_copy_set_app_name_t)dlsym(lib_cc_handle, SCC_SetAppName);
+    if (nullptr == secure_copy_set_app_name) {
+        cerr<< "Failed to get symbol " << SCC_SetAppName << ", " << dlerror() << endl;
+        return -1;
+    }
+
+    secure_copy_init = (secure_copy_init_t)dlsym(lib_cc_handle, SCC_Init);
+    if (nullptr == secure_copy_init) {
+        cerr<< "Failed to get symbol " << SCC_Init << ", " << dlerror() << endl;
+        return -1;
+    }
+
+    secure_copy_deinit = (secure_copy_deinit_t)dlsym(lib_cc_handle, SCC_Terminate);
+    if (nullptr == secure_copy_deinit) {
+        cerr<< "Failed to get symbol " << SCC_Terminate << ", " << dlerror() << endl;
+        return -1;
+    }
+
+    secure_copy_func = (secure_copy_func_t)dlsym(lib_cc_handle, SCC_Copy);
+    if (nullptr == secure_copy_func) {
+        cerr<< "Failed to get symbol " << SCC_Copy << ", " << dlerror() << endl;
+        return -1;
+    }
+
+    return 0;
+}
