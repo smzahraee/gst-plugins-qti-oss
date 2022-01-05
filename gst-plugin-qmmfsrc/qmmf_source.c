2cafb457f2959a69e6251939a660d147a5891c91
Yongsheng Li <quic_yon@quicinc.com>
2022-01-05 18:32:11 +0800

    gst: qmmfsrc: enable yuy422 uyvy format
    enable yuy422 uyvy format
    
    Change-Id: Ica1d0307e76b9743d03a79a19e34523efc477180

--- gst-plugin-qmmfsrc/qmmf_source.c
+++ gst-plugin-qmmfsrc/qmmf_source.c
@@ -25,6 +25,40 @@
 * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* Changes from Qualcomm Innovation Center are provided under the following license:
+*
+* Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted (subject to the limitations in the
+* disclaimer below) provided that the following conditions are met:
+*
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*
+*     * Redistributions in binary form must reproduce the above
+*       copyright notice, this list of conditions and the following
+*       disclaimer in the documentation and/or other materials provided
+*       with the distribution.
+*
+*     * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+*       contributors may be used to endorse or promote products derived
+*       from this software without specific prior written permission.
+*
+* NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+* GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+* HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+* ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+* GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+* IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+* OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+* IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #ifdef HAVE_CONFIG_H
@@ -144,15 +178,23 @@ static GstStaticPadTemplate qmmfsrc_video_src_template =
         GST_STATIC_CAPS (
             QMMFSRC_VIDEO_JPEG_CAPS "; "
             QMMFSRC_VIDEO_RAW_CAPS(
-#if defined(GST_VIDEO_YUY2_FORMAT_ENABLE)
+#if defined(GST_VIDEO_UYVY_FORMAT_ENABLE) && defined(GST_VIDEO_YUY2_FORMAT_ENABLE)
+                "{ NV12, NV16, YUY2, UYVY }") "; "
+#elif defined(GST_VIDEO_YUY2_FORMAT_ENABLE)
                 "{ NV12, NV16, YUY2 }") "; "
+#elif defined(GST_VIDEO_UYVY_FORMAT_ENABLE)
+                "{ NV12, NV16, UYVY }") "; "
 #else
                 "{ NV12, NV16 }") "; "
 #endif
             QMMFSRC_VIDEO_RAW_CAPS_WITH_FEATURES(
                 GST_CAPS_FEATURE_MEMORY_GBM,
-#if defined(GST_VIDEO_YUY2_FORMAT_ENABLE)
+#if defined(GST_VIDEO_UYVY_FORMAT_ENABLE) && defined(GST_VIDEO_YUY2_FORMAT_ENABLE)
+                "{ NV12, NV16, YUY2, UYVY }") "; "
+#elif defined(GST_VIDEO_YUY2_FORMAT_ENABLE)
                 "{ NV12, YUY2 }") "; "
+#elif defined(GST_VIDEO_UYVY_FORMAT_ENABLE)
+                "{ NV12, UYVY }") "; "
 #else
                 "{ NV12 }") "; "
 #endif
